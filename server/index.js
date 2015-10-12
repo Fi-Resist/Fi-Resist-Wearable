ca4833a275c972af88c0755e9476e4bf55bf4aaf
Daniel <dan.m.bartel@gmail.com>
2015-10-12 16:10:36 -0500

    add socket events for new and updated firefighters

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var socketHandler = require("./socketHandler/socketHandler");
 var SOCKET_EVT = require("./constant/constant").socket;
+var FIREFIGHTERS = [];
+
+
 
 app.use(express.static("public"));
-
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/public/index.html");
@@ -15,9 +17,18 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
 	console.log("a user connected");
 
-	//get msg from firefighter app
-	socket.on(SOCKET_EVT.RECEIVE.firefighter, function(msg) {
-		socketHandler.emitData(io, msg, SOCKET_EVT.SEND.officer);
+	// receive a new firefighter
+	socket.on(SOCKET_EVT.receive.NEW, function(msg) {
+		FIREFIGHTERS.push(msg);
+		socketHandler.emitData(io, FIREFIGHTERS, SOCKET_EVT.send.UPDATE);
+	});
+
+	//update a firefighter's data
+	socket.on(SOCKET_EVT.receive.UPDATE, function(msg) {
+		var index = _.findIndex(FIREFIGHTERS, {id: msg.id});
+		FIREFIGHTERS[index] = msg;
+
+		socketHandler.emitData(io, FIREFIGHTERS, SOCKET_EVT.send.UPDATE);
 	});
 });
 
