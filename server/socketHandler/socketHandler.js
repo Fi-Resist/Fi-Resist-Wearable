7797f56cf59f17dbfb2138d190f53762cfe1b6d2
Daniel <dan.m.bartel@gmail.com>
2015-10-12 16:10:52 -0500

    fix socketHandler

--- server/socketHandler/socketHandler.js
+++ server/socketHandler/socketHandler.js
@@ -1,7 +1,7 @@
 var _ = require("lodash");
 var SOCKET_EVENT = require("../constant/constant").socket;
 var emitData = function(io, data, socket_evt) {
-	if (_.includes(_.values(SOCKET_EVENT.SEND), socket_evt)) {
+	if (_.includes(_.values(SOCKET_EVENT.send), socket_evt)) {
 		io.emit(socket_evt, data);
 	}
 };
