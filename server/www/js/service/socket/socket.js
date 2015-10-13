b9a158aec163ca3f14db54b6a97defb80942c239
Daniel <dan.m.bartel@gmail.com>
2015-10-13 08:58:19 -0500

    forward socket events onto

--- server/www/js/service/socket/socket.js
+++ server/www/js/service/socket/socket.js
@@ -1,8 +1,15 @@
+// Factory exposes socket.io to angular modules
 angular.module("app")
-	.factory("socket", function(socketFactory) {
+	.factory("socket", function(socketFactory, socketEvent) {
 		var mySocket = io.connect("http://localhost:3000/");
-		return socketFactory({
+
+		var sock = socketFactory({
 			ioSocket: mySocket
 		});
+
+		// forward socket events to $scope events
+		sock.forward(socketEvent.UPDATE);
+
+		return sock;
 	});
 
