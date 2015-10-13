7eb2efd9f1b2804f9f37cf2c847ac1d68fa393a3
Daniel <dan.m.bartel@gmail.com>
2015-10-13 11:42:37 -0500

    add firefighterDetail controller tests

---
+++ server/www/js/view/firefighterDetail/firefighterDetail.spec.js
@@ -0,0 +1,50 @@
+describe("firefighterDetail controller", function() {
+	var scope, firefightersServiceStub, rootScope, _event;
+
+	beforeEach(function() {
+		module("app");
+		module("templates");
+		inject(function($rootScope, $controller, event) {
+			scope = $rootScope.$new();
+			_event = event;
+			rootScope = $rootScope;
+
+			firefightersServiceStub = {
+				get: function(id) {
+					return {
+						id: id
+					};
+				}
+			};
+
+			$controller("firefighterDetailCtrl", {
+				$scope: scope,
+				$stateParams: {id: 1},
+				firefighters: firefightersServiceStub
+			});
+		});
+	});
+
+	describe("event listeners", function() {
+		describe("enter view", function() {
+			beforeEach(function() {
+				rootScope.$broadcast("$ionicView.beforeEnter");
+				scope.$apply();
+			});
+			it("should put data on the scope", function() {
+				assert.equal(scope.firefighter.id, 1);
+			});
+		});
+
+		describe("update event", function() {
+			beforeEach(function() {
+				rootScope.$broadcast(_event.SCOPE_UPDATE);
+				scope.$apply();
+			});
+			it("should put data on the scope", function() {
+				assert.equal(scope.firefighter.id, 1);
+			});
+		});
+	});
+});
+
