c9d351339e9e7bca5f9292135ab094e45b685d1c
Daniel <dan.m.bartel@gmail.com>
2015-10-13 17:37:04 -0500

    add UPDATE event listener in firefighterList

--- server/www/js/view/firefighterList/firefighterList.js
+++ server/www/js/view/firefighterList/firefighterList.js
@@ -7,6 +7,7 @@ angular.module("app")
 
 		$scope.$on("$ionicView.beforeEnter", refreshData);
 		$scope.$on(event.SCOPE_CREATE, refreshData);
+		$scope.$on(event.SCOPE_UPDATE, refreshData);
 
 		$scope.isInDanger = function(f) {
 			return f.bpm > 110;
