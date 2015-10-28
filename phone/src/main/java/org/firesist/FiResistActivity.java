d34fd4a3526f73b9457fd73e5f33020f0e07365f
Daniel <dan.m.bartel@gmail.com>
2015-10-28 11:29:29 -0500

    phone socket connection

--- phone/src/main/java/org/firesist/FiResistActivity.java
+++ phone/src/main/java/org/firesist/FiResistActivity.java
@@ -3,24 +3,33 @@ package org.firesist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import java.net.URISyntaxException;
 
 public class FiResistActivity extends Activity {
+	private Socket socket;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.firesist_layout);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.firesist_layout);
+		try {
+			socket = IO.socket(getString(R.string.host));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		socket.connect();
+		socket.emit(getString(R.string.connect), "hi there");
     }
 
-    @Override
-    public void onStart() {
-	super.onStart();
-	TextView textView = (TextView) findViewById(R.id.text_view);
-	textView.setText("Hello");
-    }
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		socket.disconnect();
+	}
 
 
 
-    
-    
 }
