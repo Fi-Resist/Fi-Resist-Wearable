668ffdab9d2c0679781f4dae486889ffcc1c5f82
Daniel <dan.m.bartel@gmail.com>
2015-10-30 14:40:32 -0500

    add update constant

--- server/www/js/constant/event/event.js
+++ server/www/js/constant/event/event.js
@@ -1,6 +1,7 @@
 // Constant for socket events
 angular.module("app.constants")
 	.constant("event", {
+		SOCKET_CREATE: "officer-update",
 		SOCKET_UPDATE: "officer-update",
 		SCOPE_UPDATE: "scope:update",
 		SCOPE_CREATE: "scope:create"
