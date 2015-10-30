fc836c9b5ac7ec98a1dbb9f487c831ef223d7cee
Daniel <dan.m.bartel@gmail.com>
2015-10-30 14:41:09 -0500

    remove prototype

--- protoype/www/js/firefighterDetail/firefighterDetail.js
+++
@@ -1,7 +0,0 @@
-angular.module("app")
-	.controller("firefighterDetailCtrl", function($scope, $stateParams, firefighters) {
-		$scope.$on("$ionicView.beforeEnter", function() {
-			$scope.firefighter = firefighters.get($stateParams.id);
-			console.log($scope.firefighter);
-		});
-	});
