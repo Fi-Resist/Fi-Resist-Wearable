e1b7c5671b6dadbb0ae329a0b074f9a63bc87a57
Daniel <dan.m.bartel@gmail.com>
2015-11-02 05:27:56 -0600

    convert FiSocketHandler to singleton

--- android-app/src/main/java/org/firesist/sockethandler/FiSocketHandler.java
+++ android-app/src/main/java/org/firesist/sockethandler/FiSocketHandler.java
@@ -9,32 +9,78 @@ import java.net.URISyntaxException;
 /**
   * Web socket communication for FiResist
   */
-public class FiSocketHandler extends SocketHandler {
+public class FiSocketHandler {
 
 	// Socket message constants
 	private final String MSG_NEW_FIREFIGHTER = "new-firefighter";
 	private final String MSG_UPDATE_FIREFIGHTER = "update-firefighter";
 	private int firefighterId;
 	private String firefighterName;
+	private static FiSocketHandler fiSocketHandlerInstance;
+	private Socket socket;
 
-	/*
+	/**
 	 * Constructor initializes socket to server url, and generates an Id
+	 * Private because this is a Singleton class
 	 * @param url Url to connect to
 	 * @param name name of the firefighter
 	 */
-	public FiSocketHandler(String url, String name) {
-		super(url);
+
+	private FiSocketHandler(String url) {
+		initSocket(url);
+	}
+
+	private FiSocketHandler() {
+	}
+
+	/**
+	 * Setter for the firefighter name, also generates an Id
+	 */
+	public void setName(String name) {
 		firefighterName = name;
 		firefighterId = name.hashCode();
 	}
 
-	/*
+	/**
+	 * Initialize socket connection
+	 */
+	public void initSocket(String url) {
+		try {
+			socket = IO.socket(url);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Emits JSON to socket server
+	 * @param eventName the event to emit
+	 * @param message the message to send
+	 */
+	private void sendJSON(String eventName, JSONObject message) {
+		socket.emit(eventName, message);
+	}
+
+	/**
+	 * Constructs FiSocketHandler Singleton
+	 */
+	public static void initInstance() {
+		if (fiSocketHandlerInstance == null) {
+			fiSocketHandlerInstance = new FiSocketHandler();
+		}
+	}
+
+	public static FiSocketHandler getInstance() {
+		return fiSocketHandlerInstance;
+	}
+
+
+	/**
 	 * Wrapper for socket connect method
 	 * Connects firefighter to server, sends name + id
 	 */
-	@Override
 	public void connect() {
-		super.connect();
+		socket.connect();
 		JSONObject newFirefighter = new JSONObject();
 		try {
 			newFirefighter.append("name", firefighterName);
@@ -46,7 +92,14 @@ public class FiSocketHandler extends SocketHandler {
 		}
 	}
 
-	/*
+	/**
+	 * Wrapper for socket's disconnect method
+	 */
+	public void disconnect() {
+		socket.disconnect();
+	}
+
+	/**
      * Sends update to server
 	 * @param json Json object to send
 	 */
