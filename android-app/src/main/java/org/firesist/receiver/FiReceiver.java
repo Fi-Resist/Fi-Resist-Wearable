0d0a856e400883754f76f9efb4d77d7c136854a7
Daniel <dan.m.bartel@gmail.com>
2015-11-02 09:23:50 -0600

    skeleton receiever code

--- android-app/src/main/java/org/firesist/receiver/FiReceiver.java
+++ android-app/src/main/java/org/firesist/receiver/FiReceiver.java
@@ -4,7 +4,7 @@ package org.firesist.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import org.firesist.sockethandler.FiSocketHandler;
 
 /**
  * Runs in background, grabbing data from devices and emitting it
@@ -20,8 +20,14 @@ public class FiReceiver extends BroadcastReceiver {
 	  */
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(context, "Running", Toast.LENGTH_SHORT)
-			.show();
+		// read data from devices here...
+		System.out.println("Running receiver...");
+
+		// send data through websocket
+		//FiSocketHandler.getInstance()
+//			.sendUpdate(data);
+
+
 	}
 }
 
