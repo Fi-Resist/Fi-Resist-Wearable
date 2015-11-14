5793ae0924abdbf0b83effeb36a65c74e7ef3b77
Daniel <dan.m.bartel@gmail.com>
2015-11-14 13:40:45 -0600

    pull bioharness stuff into it's own class

---
+++ android-app/src/main/java/org/firesist/biometric/BiometricReader.java
@@ -0,0 +1,70 @@
+package org.firesist.biometric;
+import zephyr.android.BioHarnessBT.*;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import java.lang.RuntimeException;
+import java.util.Set;
+
+public class BiometricReader {
+
+	private BTClient btClient;
+	private String bhMacId;
+	FiConnectedListener connectedListener;
+	BluetoothAdapter adapter = null;
+
+	public BiometricReader() {
+		adapter = BluetoothAdapter.getDefaultAdapter();
+	}
+
+	private void connectBioharness()  throws RuntimeException {
+		// Get the mac id
+		if (bhMacId == null) {
+			Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+						
+			if (pairedDevices.size() > 0)
+			{
+				for (BluetoothDevice device : pairedDevices)
+				{
+					if (device.getName().startsWith("BH"))
+					{
+						BluetoothDevice btDevice = device;
+						bhMacId = btDevice.getAddress();
+						break;
+					}
+				}
+			}
+		}
+
+		//if no device found
+		if (bhMacId == null) {
+			throw new RuntimeException("No BioHarness found");
+		}
+
+					
+		BluetoothDevice Device = adapter.getRemoteDevice(bhMacId);
+		btClient = new BTClient(adapter, bhMacId);
+		connectedListener= new FiConnectedListener(null);
+		btClient.addConnectedEventListener(connectedListener);
+	}
+		
+
+
+	/**
+	  * Initializes BlueTooth connection and hooks up the connected listener
+	  */
+	public void startReading() throws RuntimeException {
+		connectBioharness();
+		if (btClient.IsConnected()) {
+			btClient.start();
+		}
+	}
+
+	/**
+	  * Closes down BlueTooth connection
+	  */
+	public void stopReading() {
+		// Disconnect bioharness
+		btClient.removeConnectedEventListener(connectedListener);
+		btClient.Close();
+	}
+}
