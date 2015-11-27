39f081be9a6784af7bea34d828d7e34d8f40aa96
leolastar <plaminj@hotmail.com>
2015-11-27 12:33:49 -0600

    avatar

--- node-components/monitor-app/www/js/service/firefighter/firefighter.js
+++ node-components/monitor-app/www/js/service/firefighter/firefighter.js
@@ -4,6 +4,16 @@ angular.module("app.services")
 	.factory("firefighters", function($rootScope, event) {
 		var firefighters = [];
 
+		var firefighteravatars = [
+			{"id": 1, "pic": "F1.jpg"}
+			{"id": 2, "pic": "F2.jpg"}
+			{"id": 3, "pic": "F3.jpg"}
+			{"id": 4, "pic": "F4.jpg"}
+			{"id": 5, "pic": "F5.jpg"}
+			{"id": 6, "pic": "F6.jpg"}
+			{"id": 7, "pic": "F7.jpg"}
+		];
+
 		var get = function(id) {
 			if (id) {
 				for (var i = 0; i < firefighters.length; i++) {
@@ -36,6 +46,16 @@ angular.module("app.services")
 			firefighters = firefighterArr;
 		};
 
+		var findimagebyID = function(id) {
+			if (id) {
+				var avatar = firefighteravatars[id];
+				return avatar
+			}
+			else
+				return null;
+			
+		};
+
 		var remove = function(id) {
 			if (id) {
 				_.remove(firefighters, function(f) {
@@ -52,6 +72,7 @@ angular.module("app.services")
 			update: update,
 			create: create,
 			initFirefighterArray: initFirefighterArray,
-			remove: remove
+			remove: remove,
+			findimagebyID; findimagebyID
 		};
 	});
