bda5e1fe52fd334a4e52cd91873c0b8368ae214d
Daniel <dan.m.bartel@gmail.com>
2015-11-28 11:19:59 -0600

    add firefighter form submission

--- node-components/server/static/app/js/view/roster/roster.js
+++ node-components/server/static/app/js/view/roster/roster.js
@@ -1,7 +1,15 @@
 angular.module("app.controllers")
-	.controller("RosterCtrl", function($scope) {
+	.controller("RosterCtrl", function($scope, $state, firefighter) {
+
+		// Form submission fn: add a new firefighter
 		$scope.submit = function() {
 			alert("submit!");
+			if ($scope.newFirefighter) {
+				firefighter.post({name: $scope.newFirefighter})
+					.then(function() {
+						$state.go(".", {}, {reload: true});
+					});
+			}
 		}
 	});
 
