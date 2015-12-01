0c34fe2977046b6b891f3e3539d2f3c0355e61d3
Daniel <dan.m.bartel@gmail.com>
2015-11-30 19:44:14 -0600

    add distance calculation to accelerometer reader

--- android-app/src/main/java/org/firesist/position/AccelerometerReader.java
+++ android-app/src/main/java/org/firesist/position/AccelerometerReader.java
@@ -24,24 +24,56 @@ public class AccelerometerReader implements SensorEventListener {
 	private SensorManager sensorManager;
 	private Sensor accelSensor;
 	private float[] accelValues = new float[3];
+	private long pastTime;
+	private long startTime;
+	private float lastAccel;
 
 	public AccelerometerReader(Context context)
 	{
 		sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+		startTime = pastTime;
+		lastAccel = 0;
 
-		if(sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size()>0)
+		if(sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size()>0) {
 			accelSensor = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+		}
+
+	}
+
+
+	private float calcDistance(float newAccel, long timeElapsed) {
+		float a = (timeElapsed) / (timeElapsed + (5));
+		return (a * lastAccel) + (1 - a) * newAccel; 
+	}
+
+	public void startListening() {
+
+		pastTime = System.currentTimeMillis();
+		sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	public void stopListening() {
+		sensorManager.unregisterListener(this);
 	}
 
 	@Override 
 	public void onSensorChanged(SensorEvent event)
 	{
 		Sensor mySensor = event.sensor;
-		Log.d("FIRESIST", String.format("Accel: %f, %f, %f", event.values[0], event.values[1], event.values[2]));
 
 		if(mySensor.getType() == Sensor.TYPE_ACCELEROMETER)
 		{
-			accelValues = event.values.clone();
+			accelValues[0] = event.values[0];
+			accelValues[1] = event.values[1];
+			accelValues[2] = event.values[2];
+			if (System.currentTimeMillis() >= (pastTime + (5 * 1000))) {
+				pastTime = System.currentTimeMillis();
+				Log.d("ACCEL", "5 seconds has passed");
+				Log.d("ACCEL", String.format("%f", calcDistance(event.values[2], pastTime)));
+				lastAccel = event.values[2];
+			}
+
+
 		}
 	}	
 
