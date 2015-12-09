152c274c3083fd828c49093d9acdcefef780b7a5
Daniel <dan.m.bartel@gmail.com>
2015-12-09 12:07:20 -0600

    navigation & biometrics display

--- node-components/monitor-app/www/js/view/firefighterDetail/firefighterDetail.js
+++ node-components/monitor-app/www/js/view/firefighterDetail/firefighterDetail.js
@@ -2,7 +2,33 @@ angular.module("app.controllers")
 	.controller("firefighterDetailCtrl", function($scope, $stateParams, firefighters, event) {
 
 		var refreshData = function() {
-			$scope.firefighter = firefighters.get($stateParams.id);
+			var firefighter= firefighters.get($stateParams.id);
+
+			$scope.firefighter = {
+				bio: firefighter.bio
+			};
+
+
+			$scope.firefighter.position = _.map(firefighter.position, function(p, index) {
+				var direction = "";
+				var orientation = p.orientation;
+				if (index == 0) {
+					direction = "START";
+				}
+				else {
+					var diff = p.orientation - firefighter.position[index - 1].orientation;
+					direction = (diff > 0) ? "LEFT" : "RIGHT";
+
+					orientation = Math.abs(diff); 
+
+				}
+
+				return {
+					pos: p.pos,
+					orientation: orientation,
+					direction: direction
+				};
+			});
 		};
 
 		$scope.$on("$ionicView.beforeEnter", refreshData);
--- node-components/monitor-app/www/js/view/firefighterList/firefighterList.js
+++ node-components/monitor-app/www/js/view/firefighterList/firefighterList.js
@@ -1,16 +1,23 @@
 angular.module("app.controllers")
-	.controller("firefighterListCtrl", function($scope, firefighters, event) {
+	.controller("firefighterListCtrl", function($scope, $window, firefighters, event) {
 
 		var refreshData = function() {
 			$scope.firefighters = firefighters.get();
 		};
+
 
 		$scope.$on("$ionicView.beforeEnter", refreshData);
 		$scope.$on(event.SCOPE_CREATE, refreshData);
 		$scope.$on(event.SCOPE_UPDATE, refreshData);
 
 		$scope.isInDanger = function(f) {
-			return f.bpm > 110;
+			return f.bio.bpm > 210;
 		};
+
+
+		$scope.isWarn = function(f) {
+			return (f.bio.bpm >= 192 && f.bio.bpm <= 210);
+		}
+
 	});
 
